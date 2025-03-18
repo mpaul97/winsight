f0108913f517a5e1b21154e13290d44955d67a40
Michael Paul <michaelpaul@Mac.grandenetworks.net>
2025-03-18 13:04:02 -0500

    player table

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
+import "primeicons/primeicons.css";
 import './assets/main.css';
 import router from './router';
+
 import { createApp } from 'vue';
+import PrimeVue from "primevue/config";
+import ConfirmationService from 'primevue/confirmationservice'
+import DialogService from 'primevue/dialogservice'
+import ToastService from 'primevue/toastservice';
+
+import Noir from "./presets/Noir";
+import AppState from "./plugins/appState";
 import App from './App.vue';
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+      preset: Noir,
+      options: {
+          prefix: 'p',
+          cssLayer: false,
+      }
+  }
+});
+app.use(AppState);
+app.use(ConfirmationService);
+app.use(ToastService);
+app.use(DialogService);
+app.use(router);
+
+app.mount("#app");
